4ac95c7c65a9e9f281c8d1a84706de843ecbc7ae
Natalya Ivanova <natalyas.ivanova@gmail.com>
2021-07-09 22:39:27 +0300

    Initial commit

---
+++ src/exercise/03-01.js
@@ -0,0 +1,46 @@
+// Lifting state
+// http://localhost:3000/isolated/final/03.js
+
+import * as React from 'react'
+
+function Name() {
+  const [name, setName] = React.useState('')
+  function onNameChange (event) {
+    setName(event.target.value)
+  }
+  return (
+    <div>
+      <label htmlFor="name">Name: </label>
+      <input id="name" value={name} onChange={onNameChange} />
+    </div>
+  )
+}
+
+function FavoriteAnimal({animal, onAnimalChange}) {
+  return (
+    <div>
+      <label htmlFor="animal">Favorite Animal: </label>
+      <input id="animal" value={animal} onChange={onAnimalChange} />
+    </div>
+  )
+}
+
+function Display({animal}) {
+  return <div>{`Your favorite animal is: ${animal}!`}</div>
+}
+
+function App() {
+  const [animal, setAnimal] = React.useState('')
+  return (
+    <form>
+      <Name />
+      <FavoriteAnimal
+        animal={animal}
+        onAnimalChange={event => setAnimal(event.target.value)}
+      />
+      <Display animal={animal} />
+    </form>
+  )
+}
+
+export default App
